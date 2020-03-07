bd174b0b5f70f5932ddd9f2855b6881d00373a83
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2020-03-07 13:12:53 -0500

    Potentiometer based limits for Turret
    LEFT_LIMIT and RIGHT_LIMIT values not real!

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -11,11 +11,16 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Turret extends SubsystemBase {
   private WPI_TalonSRX motor;
+  private AnalogPotentiometer pot;
+  
+  private double LEFT_LIMIT = .4;
+  private double RIGHT_LIMIT = .6; 
 
   /**
    * Creates a new Turret.
@@ -24,16 +29,26 @@ public class Turret extends SubsystemBase {
     motor = new WPI_TalonSRX(9);
     motor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 0);
     motor.setSelectedSensorPosition(0);
+    pot = new AnalogPotentiometer(2); 
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     SmartDashboard.putNumber("Turret Encoder", motor.getSelectedSensorPosition());
+    SmartDashboard.putNumber("Turret Pot", pot.get());
+    final double position = pot.get();
+    if (position < LEFT_LIMIT || position > RIGHT_LIMIT) {
+      motor.stopMotor();
+    }
+
   }
 
   public void set(double speed) {
-    motor.set(ControlMode.PercentOutput, speed);
+    final double position = pot.get();
+    if (position > LEFT_LIMIT && position < RIGHT_LIMIT) {
+      motor.set(ControlMode.PercentOutput, speed);
+    }
   }
 
   public double getEncoderValue() {
